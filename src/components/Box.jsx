7e3900bf60210ebfe145fd67a80f919e42eba001
Alex <alexander.petrov420@gmail.com>
2024-01-31 21:46:25 +0000

    test

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -2,6 +2,7 @@ import { useRef, useState } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { useRecoilValue } from "recoil";
+import { useEffect } from 'react';
 
 import { getAcceState } from "./Accelerometer";
 import {
@@ -16,29 +17,21 @@ function Box(props) {
   const [hovered, hover] = useState(null);
   const [clicked, click] = useState(false);
   const xyz = useRecoilValue(getAcceState);
-
+  const { position } = props;
+ 
   console.log(hovered);
   useFrame((state, delta) => {
     ref.current.rotation.x = ref.current.rotation.y += delta;
-    if (clicked) {
-      if (xyz.x > 0.1) {
-        ref.current.position.x = ref.current.position.x + delta;
-      }
-      if (xyz.x < -0.1) {
-        ref.current.position.x = ref.current.position.x - delta;
-      }
-      if (xyz.y > 0.1) {
-        ref.current.position.x = ref.current.position.y + delta;
-      }
-      if (xyz.y < -0.1) {
-        ref.current.position.x = ref.current.position.y - delta;
-      }
+    if (clicked && xyz.x > 0) {
+      console.log(position)
+      ref.current.position.x += delta
     }
   });
   return (
     <Select enabled={clicked}>
       <mesh
         ref={ref}
+        position={[position.x, position.y, position.z]}
         {...props}
         onPointerOver={() => hover(true)}
         onPointerOut={() => hover(false)}
