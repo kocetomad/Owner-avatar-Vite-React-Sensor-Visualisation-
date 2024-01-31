86f44620570107937388dd983a2a0b8c4b2e4cf9
Alex <alexander.petrov420@gmail.com>
2024-01-31 21:50:46 +0000

    testing accel sensitivity

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -22,7 +22,7 @@ function Box(props) {
   console.log(hovered);
   useFrame((state, delta) => {
     ref.current.rotation.x = ref.current.rotation.y += delta;
-    if (clicked && xyz.x < 0.2) {
+    if (clicked && xyz.x < -0.25) {
       console.log(position)
       ref.current.position.x += delta
     }
